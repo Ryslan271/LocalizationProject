252a903ee7357c587ea18ff0e36d46f435c14bce
Ryslan271 <89272413062@mail.ru>
2024-01-22 23:34:47 +0300

    Creating a class that creates a dynamic class and adds the necessary properties to it with the given ones (property name and type)

--- LocalizationProject/MainWindow.xaml.cs
+++ LocalizationProject/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace LocalizationProject
 {
@@ -16,9 +11,66 @@ namespace LocalizationProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private (string KeyPrefix, string Key, string Text) list = new();
+        private GridView gridView = new();
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gridView.Columns.Add(BuildingGridViewColumn("Key"));
+            ListTable.View = gridView;
+        }
+
+        /// <summary>
+        /// Формирование таблицы
+        /// </summary>
+        private void BuildingTable()
+        {
+
+        }
+
+        /// <summary>
+        /// Создание наименования колонок
+        /// </summary>
+        /// <param name="bindingName">Строка подключения/Наименование колонки</param>
+        /// <returns>Колонка</returns>
+        private GridViewColumn BuildingGridViewColumn(string bindingName) =>
+            new(){ Header = bindingName, DisplayMemberBinding = new Binding(bindingName) };
+
+        /*
+         * <ResourceDictionary xmlns="https://github.com/avaloniaui"
+                    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+                    xmlns:system="clr-namespace:System;assembly=System.Runtime">
+
+                <system:String x:Key="{Key-Prefix}.{Key}">{Text}</system:String>
+
+            </ResourceDictionary>
+        */
+
+        /// <summary>
+        /// Создание необходим  ой строки
+        /// </summary>
+        /*<system:String x:Key="{Key-Prefix}.{Key}">{Text}</system:String>*/
+        private string BuildingStrings() =>
+            $"<system:String x:Key=\"{list.KeyPrefix}.{list.Key}\">{list.Text}</system:String>";
+
+        /// <summary>
+        /// Сформированные таблицы
+        /// </summary>
+        private void Button_Click(object sender, RoutedEventArgs e) =>
+            BuildingTable();
+
+        /// <summary>
+        /// Добавление новой колонки
+        /// </summary>
+        private void AddLanguage(object sender, RoutedEventArgs e)
+        {
+            if (Language.Text == "" || Language.Text == null)
+                return;
+
+            gridView.Columns.Add(BuildingGridViewColumn(Language.Text.Trim()));
         }
     }
 }
---
+++ LocalizationProject/UnknownClass.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace LocalizationProject
+{
+    public static class UnknownClass
+    {
+        /// <summary>
+        /// Создание динамического класса
+        /// </summary>
+        /// <param name="className">Наименование класса</param>
+        /// <param name="properties">Свойства (название свойства, тип свойства)</param>
+        /// <returns>object - созданный класс</returns>
+        public static object BuildingClass(string className, List<(string property, Type type)> properties)
+        {
+            TypeBuilder dynamicClass = BuildingType(className);
+
+            foreach (var (property, type) in properties)
+                AddProperty(dynamicClass, property, type);
+
+            return Activator.CreateInstance(dynamicClass.CreateType())!;
+        }
+
+        /// <summary>
+        /// Получение значение свойства класса
+        /// </summary>
+        /// <param name="currentClass">Класс</param>
+        /// <param name="propertyName">Наименование свойства</param>
+        /// <returns></returns>
+        public static object GetProperty(object currentClass, string propertyName)
+        {
+            try
+            {
+                return currentClass.GetType().GetProperty(propertyName)!.GetValue(currentClass)!;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Изменение значение свойства
+        /// </summary>
+        /// <typeparam name="T">Неизвестный тип нового значений свойства</typeparam>
+        /// <param name="currentClass">Класс</param>
+        /// <param name="propertyName">Наименование свойства</param>
+        /// <param name="value">Значение свойства с неизвестным типом</param>
+        /// <returns>True\Сообщение об ошибке</returns>
+        public static object SetProperty<T>(object currentClass, string propertyName, T value)
+        {
+            try
+            {
+                currentClass.GetType().GetProperty(propertyName)!.SetValue(currentClass, value);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Создание динамического класса
+        /// </summary>
+        /// <param name="className">Название будущего класса</param>
+        /// <returns></returns>
+        private static TypeBuilder BuildingType(string className)
+        {
+            // Создание динамической сборки
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("DynamicAssembly"), AssemblyBuilderAccess.Run);
+
+            return assemblyBuilder.DefineDynamicModule("DynamicModule").DefineType(className, TypeAttributes.Public);
+        }
+
+        /// <summary>
+        /// Добавление свойства в класс
+        /// </summary>
+        /// <param name="typeBuilder">Динамический класс</param>
+        /// <param name="propertyName">Название свойства</param>
+        /// <param name="propertyType">Значение свойства</param>
+        private static void AddProperty(TypeBuilder typeBuilder, string propertyName, Type propertyType)
+        {
+            var fieldBuilder = typeBuilder.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            var propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.None, propertyType, null);
+
+            var getMethodBuilder = typeBuilder.DefineMethod("get_" + propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, propertyType, Type.EmptyTypes);
+            var getILGenerator = getMethodBuilder.GetILGenerator();
+            getILGenerator.Emit(OpCodes.Ldarg_0);
+            getILGenerator.Emit(OpCodes.Ldfld, fieldBuilder);
+            getILGenerator.Emit(OpCodes.Ret);
+
+            var setMethodBuilder = typeBuilder.DefineMethod("set_" + propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, null, new[] { propertyType });
+            var setILGenerator = setMethodBuilder.GetILGenerator();
+            setILGenerator.Emit(OpCodes.Ldarg_0);
+            setILGenerator.Emit(OpCodes.Ldarg_1);
+            setILGenerator.Emit(OpCodes.Stfld, fieldBuilder);
+            setILGenerator.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getMethodBuilder);
+            propertyBuilder.SetSetMethod(setMethodBuilder);
+        }
+    }
+}
